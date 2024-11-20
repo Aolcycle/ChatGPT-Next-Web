815e7546b262900108c56ad1419c4b8b806ecffd
敖烈 <2839882709@qq.com>
2024-11-20 09:54:04 +0800

    溢出检测更新; 解决bug

--- app/constant.ts
+++ app/constant.ts
@@ -1,4 +1,4 @@
-export const OWNER = "Aolcycle";
+export const OWNER = "ChatGPTNextWeb";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const PLUGINS_REPO_URL = `https://github.com/${OWNER}/NextChat-Awesome-Plugins`;
